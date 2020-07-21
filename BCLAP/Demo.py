5e5a63b4c5ab6795d3a4672b2d180a956cf8a5a7
Nawaz Akhtar <nawaz.akhtar@kaplan.edu>
2020-07-21 17:38:14 +0530

    Commit

--- BCLAP/Demo.py
+++ BCLAP/Demo.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from time import sleep
 
 chromeDriver = webdriver.Chrome('C:\Driver\chromedriver.exe')
 
@@ -9,3 +12,7 @@ searchBtn=chromeDriver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[3
 searchBtn.click()
 searchBtn=chromeDriver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div[1]/a')
 searchBtn.click()
+chromeDriver.find_element_by_xpath('//*[@id="ss"]').send_keys('London')
+sleep(2)
+searchBox.send_keys(Keys.ENTER)
+searchBox.send_keys(Keys.TAB)
